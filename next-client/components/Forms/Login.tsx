f85ce4e4d9607f1632d7183024b9830fa9d84339
Michael Velsigne <mjg.grant.m@gmail.com>
2022-10-19 21:41:25 +1100

    Testing initial page structure

---
+++ next-client/components/Forms/Login.tsx
@@ -0,0 +1,9 @@
+interface Props {
+    
+}
+
+const Login = ({}:Props) =>{
+    return (<></>)
+}
+
+export default Login
---
+++ next-client/pages/login/index.tsx
@@ -0,0 +1,15 @@
+const Login = () => {
+    return (
+        <>
+        <form>
+            <input>
+            </input>
+            <input>
+            </input>
+            <button></button>
+        </form>
+        </>
+    )
+}
+
+export default Login; 
